992c4bf8c6342ab734d13dad0c80603265c98f47
mebusy <qibinyi@mebusys-iMac.ubisoft.org>
2023-07-27 15:47:49 +0800

    + 1.basic pygame window

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# pyMinecraft
-learning coding minecraft in pygame
+# Creating a Voxel Engine (like Minecraft) from Scratch in Python
+
+https://www.youtube.com/watch?v=Ab8TOSFfNp4
+
+
+## libraries
+
+```bash
+pip install pygame moderngl PyGLM numba
+```
+
+library | desc
+--- | ---
+modergl | a python wrapper over OpenGL 3.3+ core that simplifies the creation of simple graphics applications like scientific simulations, games or user interfaces. 
+PyGLM | OpenGL Mathematics (GLM) library for Python, to fill in the programming gaps in writing the basic vector and matrix mathematics for OpenGL applications. <br> https://web.engr.oregonstate.edu/~mjb/cs491/Handouts/GLM.1pp.pdf
+numba | JIT compiler that translates a subset of Python and NumPy code into fast machine code.
+
+
+
+
+
+
---
+++ minecraft/main.py
@@ -0,0 +1,79 @@
+#!python3
+
+from settings import *
+import moderngl as mgl
+import pygame as pg
+import sys
+
+
+class VoxelEngine:
+    def __init__(self):
+        # initialize pygame
+        pg.init()
+        # use opengl 3.3
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+        pg.display.gl_set_attribute(
+            pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE
+        )
+        pg.display.gl_set_attribute(pg.GL_DEPTH_SIZE, 24)
+
+        # set window resolution
+        pg.display.set_mode(WIN_RES, flags=pg.DOUBLEBUF | pg.OPENGL)
+
+        # create the openGL context itself
+        self.ctx = mgl.create_context()
+        # active fragment depth testing, culling and blending
+        self.ctx.enable(mgl.DEPTH_TEST | mgl.CULL_FACE | mgl.BLEND)
+        # enable automatic garbage collection of unused opengl objects (thx to mgl)
+        self.ctx.gc_mode = "auto"
+
+        # keep track of time
+        self.clock = pg.time.Clock()
+        self.delta_time: float = 0.0
+        self.time: float = 0.0
+
+        # a flag to check if the game is running
+        self.is_running: bool = True
+
+    # updating the state of objects
+    def update(self):
+        # update delta time
+        # clock.tick() should be called once per frame. It will compute how many milliseconds have passed since the previous call.
+        self.delta_time = self.clock.tick()
+        # time.get_ticks() returns the number of milliseconds since pygame.init()
+        self.time = pg.time.get_ticks() * 0.001
+
+        # display fps in title
+        pg.display.set_caption(f"FPS: {self.clock.get_fps():.0f}")
+
+    # render game objects
+    def render(self):
+        # clear the frame and depth buffers
+        self.ctx.clear(color=BG_COLOR)
+        pg.display.flip()
+
+    # handle events
+    def handle_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (
+                event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE
+            ):
+                self.is_running = False
+
+    def run(self):
+        # main loop
+        while self.is_running:
+            self.handle_events()
+            self.update()
+            self.render()
+
+        # quit pygame
+        pg.quit()
+        # quit the program
+        sys.exit()
+
+
+if __name__ == "__main__":
+    app = VoxelEngine()
+    app.run()
