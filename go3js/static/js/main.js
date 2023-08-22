330fe63a9189af136c8cdffcfb2bbfdcddec22f0
mebusy <qibinyi@mebusys-iMac.ubisoft.org>
2023-08-22 19:22:03 +0800

    * obit control

--- go3js/static/js/main.js
+++ go3js/static/js/main.js
@@ -1,5 +1,8 @@
 import * as THREE from "three";
 
+// control
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -11,6 +14,9 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+// control
+const controls = new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
